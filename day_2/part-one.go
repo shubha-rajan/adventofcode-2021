99b86f3cbd5054fed063bb40b37790ce0850cdfd
Shubha Rajan <shubhadayini@google.com>
2021-12-01 21:13:50 -0800

    solve part one day 2

---
+++ day_2/part-one.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var depth int
+	var pos int
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	sc := bufio.NewScanner(file)
+	for sc.Scan() {
+		move := strings.Fields(sc.Text())
+		dist, err := strconv.Atoi(move[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch move[0] {
+		case "forward":
+			pos += dist
+		case "down":
+			depth += dist
+		case "up":
+			depth -= dist
+		}
+	}
+	fmt.Printf("depth: %d\n", depth)
+	fmt.Printf("posn: %d\n", pos)
+	fmt.Printf("product: %d\n", pos*depth)
+}
